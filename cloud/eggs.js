36897857863fda568adb66b46f65c062808f9bdf
Nico <nicoconstantin11@gmail.com>
2021-11-22 13:22:19 -0300

    mint_egg working

--- cloud/eggs.js
+++ cloud/eggs.js
@@ -1,10 +1,20 @@
-const logger = Moralis.Cloud.getLogger();
-
-Moralis.Cloud.define('mint_egg', async (request) => {
+Moralis.Cloud.define('mint_egg', async (req) => {
     const Egg = Moralis.Object.extend('Egg');
-    const newEgg = new Egg();
-    newEgg.set('timeHatch', getDate(2,"hours"))
-    newEgg.set('isHatched', false)
-    return newEgg.save()
+    const query = new Moralis.Query(Moralis.User);
+    
+    try{
+        let actualUser = await query.get( req.user.id, { useMasterKey:true } )
+        const newEgg = new Egg();
+        newEgg.set('timeHatch', getDate(2,"hours"))
+        newEgg.set('isHatched', false)
+        newEgg.set('owner', actualUser)
+        await newEgg.save()
+        return "egg created"
+    }
+
+    catch(error){
+        return error.message
+    }
+    
     
 });
