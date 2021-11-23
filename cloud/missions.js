608d8cfe7ee67b894c770aeaa9176daba65603ab
Nico <nicoconstantin11@gmail.com>
2021-11-23 14:49:03 -0300

    do creator quest

--- cloud/missions.js
+++ cloud/missions.js
@@ -15,3 +15,31 @@ Moralis.Cloud.define('get_missions', async (req) => {
         }
     }
 });
+
+Moralis.Cloud.define('do_creator_quest', async (req) => {
+    const query_economy = new Moralis.Query('ECONOMY');
+    const query_user = new Moralis.Query(Moralis.User);
+
+    try {
+        query_economy.equalTo('reference','reward_per_avatar_party')
+        let price_per_avatar = await query_economy.find()
+        const actualUser = await query_user.get(req.user.id, { useMasterKey:true })
+        let amountWon = price_per_avatar[0].attributes.price * req.params.qty_avatars
+        actualUser.set('balanceClaim', actualUser.attributes.balanceClaim + amountWon)
+        actualUser.set('timeQuest', getDate(1,'days'))
+        let userUpdated = await actualUser.save(null, { useMasterKey:true })
+
+        return {
+            new_balance: userUpdated.attributes.balanceClaim,
+            message: "Creator quest done"
+        }
+        
+    } catch (error) {
+
+        return {
+            new_balance: false,
+            message: error.message
+        }
+        
+    }
+});
