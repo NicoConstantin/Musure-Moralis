35d4dec641c8b2c5ef9188bd9fe76fdfc1bb7d5f
Nico <nicoconstantin11@gmail.com>
2021-11-23 10:27:10 -0300

    patch_party_data

--- cloud/parties.js
+++ cloud/parties.js
@@ -1,3 +1,25 @@
-Moralis.Cloud.define('create_party', async (request) => {
-    
+const Party = Moralis.Object.extend('Party');
+
+
+Moralis.Cloud.define('patch_party_data', async (req) => {
+    try {
+        const query_user = new Moralis.Query(Moralis.User);
+        const actualUser = await query_user.get(req.user.id, { useMasterKey:true });
+        const newParty = new Party();
+        newParty.set('name', req.params.name);
+        newParty.set('bio', req.params.bio);
+        newParty.set('image',req.params.image);
+        newParty.set('owner', actualUser);
+        await newParty.save()
+
+        return {
+            updated:true,
+            message: "Party Updated"
+        }
+    } catch (error) {
+        return {
+            updated:true,
+            message: error.message
+        }
+    }
 });
